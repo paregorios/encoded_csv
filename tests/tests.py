f838c4e2ef435ac64edb314a80fa0013b3af1a44
Tom Elliott <tom.elliott@nyu.edu>
2017-04-27 11:11:43 -0500

    all possible data saved from excel, plus tests for mac and msdos and tabs; tabs failing because we're not yet supporting dialect detection

--- tests/tests.py
+++ tests/tests.py
@@ -5,18 +5,46 @@ __copyright__ = 'Copyright ©️ 2017 New York University'
 __license__ = 'See LICENSE.txt'
 __version__ = '0.1'
 
+from inspect import getsourcefile
 from nose.tools import assert_equal, assert_not_equal, raises
 from encoded_csv import get_csv
+from os.path import dirname, join as joinpath, realpath
 import unittest
 
 
-class SpaceTests(unittest.TestCase):
+class CSVTests(unittest.TestCase):
 
     def setUp(self):
-        self.data_dir = 'The quick brown fox jumped over the lazy sea urchin.'
+        self.data_dir = joinpath(
+            dirname(realpath(getsourcefile(lambda: 0))), 'data')
+        self.fields = [
+            'pet_id',
+            'species',
+            'name',
+            'nicknames',
+            'behaviors',
+            'weight'
+        ]
+        self.length = 5
 
     def tearDown(self):
         pass
 
-    def test_empty(self):
-        pass
+    def _execute(self, **kwargs):
+        kwargs['csv_file'] = joinpath(self.data_dir, kwargs['csv_file'])
+        fields, data = get_csv(**kwargs)
+        assert_equal(self.length, len(data))
+        assert_equal(self.fields, fields)
+
+    def test_excel(self):
+        kwargs = {'csv_file': 'pets_excel.csv'}
+        self._execute(**kwargs)
+
+    def test_excel_msdos(self):
+        kwargs = {'csv_file': 'pets_excel_msdos.csv'}
+        self._execute(**kwargs)
+
+    def test_excel_tabs(self):
+        kwargs = {'csv_file': 'pets_excel_tabs.txt'}
+        self._execute(**kwargs)
+
