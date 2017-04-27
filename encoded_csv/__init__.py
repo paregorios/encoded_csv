9c32354d714fc210a92acd1fdd2622a7b2d87be1
Tom Elliott <tom.elliott@nyu.edu>
2017-04-27 11:59:03 -0500

    header skipping and dialect detection

--- encoded_csv/__init__.py
+++ encoded_csv/__init__.py
@@ -12,7 +12,13 @@ from itertools import islice
 import os
 
 
-def get_csv(csv_file, skip_header_lines=0, encoding=''):
+def get_csv(
+    csv_file,
+    skip_lines=0,
+    encoding='',
+    dialect='',
+    fieldnames=[]
+):
     """Read content from an encoded CSV file.
 
     We assume first row (after discarding any header lines) has column names.
@@ -31,21 +37,39 @@ def get_csv(csv_file, skip_header_lines=0, encoding=''):
     """
     rpath = os.path.realpath(csv_file)
 
-    # try to guess the encoding, if necessary
+    reader_kwargs = {}
+
+    # encoding detection
     if encoding == '':
         num_bytes = min(32, os.path.getsize(rpath))
         raw = open(rpath, 'rb').read(num_bytes)
         if raw.startswith(codecs.BOM_UTF8):
-            e = 'utf-8-sig'
+            file_encoding = 'utf-8-sig'
         else:
-            e = chardet.detect(raw)['encoding']
+            file_encoding = chardet.detect(raw)['encoding']
     else:
-        e = encoding
+        file_encoding = encoding
 
-    # open the file and read into a list of dictionaries
-    with io.open(rpath, 'r', encoding=e) as f:
+    # force fieldnames if necessary
+    if len(fieldnames) != 0:
+        reader_kwargs['fieldnames'] = fieldnames
+
+    with io.open(rpath, 'r', encoding=file_encoding) as f:
+
+        # dialect detection
+        if dialect == '':
+            if skip_lines != 0:
+                islice(f, skip_lines)
+            sample = f.read(1024)
+            sniffer = csv.Sniffer()
+            reader_kwargs['dialect'] = sniffer.sniff(sample)
+        else:
+            reader_kwargs['dialect'] = dialect
+
+        # read into a list of dictionaries
+        f.seek(0)
         content = []
-        reader = csv.DictReader(islice(f, skip_header_lines, None))
+        reader = csv.DictReader(islice(f, skip_lines, None), **reader_kwargs)
         for row in reader:
             content.append(row)
     field_names = list(content[0].keys())
