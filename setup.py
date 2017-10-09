d98bb4cde564764db023bcd95b7e866daed78497
Tom Elliott <tom.elliott@nyu.edu>
2017-10-09 16:12:26 -0500

    switch from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from codecs import open
-from distutils.core import setup
+from setuptools import setup
 from os import path
 
 here = path.abspath(path.dirname(__file__))
@@ -17,7 +17,7 @@ setup(
     version='0.2',
     description='Read a CSV file using arbitrary character encodings.',
     long_description=long_description,
-    url='http://change.me',
+    url='https://github.com/paregorios/encoded_csv',
     author='Tom Elliott',
     author_email='tom.elliott@nyu.edu',
     license='LICENSE.txt',
@@ -32,50 +32,7 @@ setup(
         'Programming Language :: Python :: 3'
     ],
     keywords='scripting, csv, i18n, encoding',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=['encoded_csv'],
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=['chardet'],
-    python_requires='~=3.6',
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    # extras_require={
-    #     'dev': ['check-manifest'],
-    #     'test': ['coverage'],
-    # },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # package_data={
-    #     'sample': ['package_data.dat'],
-    # },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
+    python_requires='~=3.6'
 )
